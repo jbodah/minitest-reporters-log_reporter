37ab578fed690364d034fd560254c3211416b393
Josh Bodah <jbodah@backupify.com>
2014-10-29 15:11:59 -0400

    initial pass

---
+++ lib/log_reporter.rb
@@ -0,0 +1,30 @@
+module Minitest
+  module Reporters
+    class LogReporter < DefaultReporter
+      # Force override the IO
+      def add_defaults(options)
+        super
+        @log_file = 'log/minitest.log'
+        @io = File.new(@log_file, 'w')
+      end
+
+      # task start
+      def start
+        super
+        $stdout.puts "Logging results to #{@log_file}\n"
+      end
+
+      # task end
+      def report
+        super
+        @io.close
+      end
+
+      private
+
+      [:green, :red, :yellow].each do |sym|
+        define_method(sym) {|str| str}
+      end
+    end
+  end
+end
